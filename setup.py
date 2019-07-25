538e172e9a3a01fe2e87bd182e741d269cb52957
Niel Venter <niel@profiteksa.com>
2019-07-25 12:45:50 +0200

    Initial

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 setuptools.setup(
-     name='mcs_lib',
-     version='1.0',
-     scripts=['mcs_lib'] ,
-     author="Niel Venter",
-     author_email="support@minelert.com",
-     description="Minelert utility package",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/javatechy/dokr",
-     packages=setuptools.find_packages(),
-     classifiers=[
+    name='mcs_lib',
+    version='1.0',
+    scripts=['mcs_lib'] ,
+    author="Niel Venter",
+    author_email="support@minelert.com",
+    description="Minelert utility package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dpventer/mcs_lib",
+    packages=setuptools.find_packages(),
+    classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
-     ],
- )
+    ],
+)
